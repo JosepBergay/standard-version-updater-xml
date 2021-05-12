e8b6c468def51a409cb4971c12f903b732fdfe2a
jberg <josep.bergay@gmail.com>
2021-05-12 21:56:33 +0200

    refactor: remove unused code

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const { create } = require('xmlbuilder2');
 
 const isVersion = key => key === 'version' || key === 'Version';
 
-const findVersion = obj => {
-  if (isVersion(obj.tagname)) {
-    return obj.val;
-  }
-  if (Array.isArray(obj)) {
-    return obj.reduce((p, c) => p ?? findVersion(c), null);
-  }
-  return obj.child && Object.keys(obj.child).reduce((p, c) => p ?? findVersion(obj.child[c]), null);
-};
-
 const readVersion = contents => {
   const doc = create(contents);
   const versionNode = doc.find(n => isVersion(n.node.nodeName), true, true);
